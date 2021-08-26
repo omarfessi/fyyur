ef5928e877623e74c07eda9ae9bc8f0ceb221f44
Omar <omarfessy@gmail.com>
2021-08-26 20:03:14 +0200

    Add show venue & show artist controllers

--- app.py
+++ app.py
@@ -178,10 +178,55 @@ def search_venues():
 
 @app.route('/venues/<int:venue_id>')
 def show_venue(venue_id):
-  # shows the venue page with the given venue_id
-  # TODO: replace with real venue data from the venues table, using venue_id
-  data1={
-    "id": 1,
+
+  venue = db.session.query(Venue).filter(Venue.id == venue_id).first()
+  if not venue:
+    return redirect(url_for('index'))
+
+  else :
+    current_time = datetime.now()
+    past_shows_results= db.session.query(Show).join(Artist).filter(Show.venue_id == venue_id).filter(Show.start_time <= current_time ).all()
+    past_shows = []
+    for show in past_shows_results :
+      past_shows.append({
+        "artist_id" : show.artist.id,
+        "artist_name" : show.artist.name,
+        "artist_image_link" : show.artist.image_link,
+        #TypeError: Parser must be a string or character stream, not datetime
+        "start_time" : show.start_time.strftime('%Y-%m-%d %H:%M:%S')
+      })
+
+    upcoming_shows_results= db.session.query(Show).join(Artist).filter(Show.venue_id == venue_id).filter(Show.start_time > current_time ).all()
+    upcoming_shows = []
+    for show in upcoming_shows_results :
+      upcoming_shows.append({
+        "artist_id" : show.artist.id,
+        "artist_name" : show.artist.name,
+        "artist_image_link" : show.artist.image_link,
+        #TypeError: Parser must be a string or character stream, not datetime
+        "start_time" : show.start_time.strftime('%Y-%m-%d %H:%M:%S')
+    })
+ 
+    data = {
+      "id" : venue.id,
+      "name": venue.name,
+      "genres": venue.genres,
+      "address": venue.address,
+      "city": venue.city,
+      "state": venue.state,
+      "phone": venue.phone,
+      "website": venue.website_link,
+      "facebook_link": venue.facebook_link,
+      "seeking_talent": venue.seeking_talent,
+      "seeking_description": venue.seeking_description,
+      "image_link": venue.image_link,
+      "past_shows": past_shows,
+      "upcoming_shows" : upcoming_shows,
+      "past_shows_count" : len(past_shows),
+      "upcoming_shows_count" : len(upcoming_shows)}
+
+    """data1={
+   "id": 1,
     "name": "The Musical Hop",
     "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
     "address": "1015 Folsom Street",
@@ -255,9 +300,7 @@ def show_venue(venue_id):
       "start_time": "2035-04-15T20:00:00.000Z"
     }],
     "past_shows_count": 1,
-    "upcoming_shows_count": 1,
-  }
-  data = list(filter(lambda d: d['id'] == venue_id, [data1, data2, data3]))[0]
+    "upcoming_shows_count": 1 } """
   return render_template('pages/show_venue.html', venue=data)
 
 #  Create Venue
@@ -342,7 +385,53 @@ def search_artists():
 def show_artist(artist_id):
   # shows the artist page with the given artist_id
   # TODO: replace with real artist data from the artist table, using artist_id
-  data1={
+
+  artist = db.session.query(Artist).filter(Artist.id == artist_id).first()
+  if not artist:
+    return redirect(url_for('index'))
+
+  else :
+    current_time = datetime.now()
+    past_shows_results= db.session.query(Show).join(Venue).filter(Show.artist_id == artist_id).filter(Show.start_time <= current_time ).all()
+    past_shows = []
+    for show in past_shows_results :
+      past_shows.append({
+        "venue_id" : show.venue.id,
+        "venue_name" : show.venue.name,
+        "venue_image_link" : show.venue.image_link,
+        #TypeError: Parser must be a string or character stream, not datetime
+        "start_time" : show.start_time.strftime('%Y-%m-%d %H:%M:%S')
+      })
+
+    upcoming_shows_results= db.session.query(Show).join(Venue).filter(Show.artist_id == artist_id).filter(Show.start_time > current_time ).all()
+    upcoming_shows = []
+    for show in upcoming_shows_results :
+      upcoming_shows.append({
+        "venue_id" : show.venue.id,
+        "venue_name" : show.venue.name,
+        "venue_image_link" : show.venue.image_link,
+        #TypeError: Parser must be a string or character stream, not datetime
+        "start_time" : show.start_time.strftime('%Y-%m-%d %H:%M:%S')
+    })
+
+    data = {
+      "id" : artist.id,
+      "name": artist.name,
+      "genres": artist.genres,
+      "city": artist.city,
+      "state": artist.state,
+      "phone": artist.phone,
+      "website": artist.website_link,
+      "facebook_link": artist.facebook_link,
+      "seeking_venue": artist.seeking_venue,
+      "seeking_description": artist.seeking_description,
+      "image_link": artist.image_link,
+      "past_shows": past_shows,
+      "upcoming_shows" : upcoming_shows,
+      "past_shows_count" : len(past_shows),
+      "upcoming_shows_count" : len(upcoming_shows)}
+
+  """data1={
     "id": 4,
     "name": "Guns N Petals",
     "genres": ["Rock n Roll"],
@@ -413,7 +502,7 @@ def show_artist(artist_id):
     "past_shows_count": 0,
     "upcoming_shows_count": 3,
   }
-  data = list(filter(lambda d: d['id'] == artist_id, [data1, data2, data3]))[0]
+  data = list(filter(lambda d: d['id'] == artist_id, [data1, data2, data3]))[0]"""
   return render_template('pages/show_artist.html', artist=data)
 
 #  Update
