b5c77e81af8d4753d47aacecbac889e470c15968
Omar <omarfessy@gmail.com>
2021-08-29 19:34:53 +0200

    Seperate Models from the main app

--- app.py
+++ app.py
@@ -24,63 +24,7 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-
-#----------------------------------------------------------------------------#
-# Models.
-#----------------------------------------------------------------------------#
-
-class Venue(db.Model):
-    __tablename__ = 'Venue'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    #add missing columns ( genre, ... )
-    genres = db.Column(db.ARRAY(db.String()))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    #add missing columns(website_link,seeking_talent,seeking_description )
-    website_link=db.Column(db.String(120))
-    seeking_talent=db.Column(db.Boolean)
-    seeking_description=db.Column(db.String(500))
-    #add relationships 
-    shows=db.relationship('Show', backref='venue', lazy=True)
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-
-class Artist(db.Model):
-    __tablename__ = 'Artist'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.ARRAY(db.String()))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    # Add missing columns
-    website_link=db.Column(db.String(120))
-    seeking_venue=db.Column(db.Boolean)
-    seeking_description=db.Column(db.String(500))
-    #add relationships 
-    shows=db.relationship('Show', backref='artist', lazy=True)
-
-
-class Show(db.Model):
-    __tablename__ = 'Show'
-
-    id = db.Column(db.Integer, primary_key=True)
-    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
-    artist_id= db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
-    start_time= db.Column(db.DateTime, nullable=False)
-
-
+from models import *
 #----------------------------------------------------------------------------#
 # Filters.
 #----------------------------------------------------------------------------#
